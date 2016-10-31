5ddac7b91b7abd39831054d10788b6b3f36e0596
Derek Kniffin <derek.kniffin@gmail.com>
2016-10-31 09:58:29 -0400

    Some cleanup

--- bin/test.sh
+++ bin/test.sh
@@ -3,7 +3,7 @@ set -x
 BEFORE_PATH=$(pwd)
 cd $QA_PATH
 
-APP_ENV=DEV xvfb-run bundle exec cucumber
+xvfb-run bundle exec cucumber
 EXIT_CODE=$?
 
 cd $BEFORE_PATH
--- features/support/env.rb
+++ features/support/env.rb
@@ -6,7 +6,7 @@ require_relative "helpers/endpoint_helper"
 require_relative "helpers/user_helper"
 
 Capybara.configure do |config|
-  config.app_host = 'http://localhost:8080' #EndpointHelper.app_base
+  config.app_host = 'http://localhost:8080'
   config.server_host = 'localhost'
   config.server_port = '8080'
   config.default_driver = :webkit
@@ -15,11 +15,6 @@ end
 
 Capybara::Webkit.configure do |config|
 end
-
-# Before do
-#   cleaner_url = "#{EndpointHelper.cleaner_base}/clean-database"
-#   Net::HTTP.get(URI.parse(cleaner_url))
-# end
 
 Capybara.ignore_hidden_elements = false
 
